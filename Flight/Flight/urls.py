2f77768132a5c6ea7347bf28e2dd16c821db9f95
saramontif <saramontif@gmail.com>
2019-01-20 14:11:33 +0200

    start active window

--- Flight/Flight/urls.py
+++ Flight/Flight/urls.py
@@ -3,12 +3,16 @@ from django.urls import path
 
 from app import views
 from app.views import main_first_window
+from app.views import active_main_window
+
 from . import view
 from app.views import *
 
 urlpatterns = [
     path('<int:question_id>/', view.detail, name='detail'),
     path('start/', main_first_window.open_window, name='open_window'),
+    path('active/', active_main_window.active_window, name='active_window'),
+
 ]
 
 
