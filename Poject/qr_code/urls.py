318675046f08646c3a2bd02563bcd2226a97b00c
saramontif <saramontif@gmail.com>
2019-01-20 13:45:37 +0200

    add qr_code.py

--- Poject/qr_code/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from qr_code import views
-
-
-app_name = 'qr_code'
-urlpatterns = [
-    url(r'^images/serve_qr_code_image/$', views.serve_qr_code_image, name='serve_qr_code_image')
-]
--- Poject/qr_code_demo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class QRCodeDemoConfig(AppConfig):
-    name = 'qr_code_demo'
-    verbose_name = 'Demo of Django QR Code'
