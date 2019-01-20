66823829d160e503adc1ff9378bc412af47ecd8e
saramontif <saramontif@gmail.com>
2019-01-20 17:13:42 +0200

    change files

--- Flight/Flight/urls.py
+++ Flight/Flight/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from app.views import main_first_window, phon_scan, active_main_window
+
+from app.views import phon_scan, active_main_window
 from app.views.main_first_window import home_view
-from . import view
 
-
-
-urlpatterns =[
+urlpatterns = [
     path('phone_scan/', phon_scan.scan, name='phon_scan'),
     path('start/', home_view.as_view(), name='start'),
     path('active/', active_main_window.active_window, name='active_window'),
 ]
-
