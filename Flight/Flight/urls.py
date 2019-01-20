8c06bd06c91160764de7914f4b1a586290ea3719
saramontif <saramontif@gmail.com>
2019-01-20 16:03:38 +0200

    template of HTML page

--- Flight/Flight/urls.py
+++ Flight/Flight/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from app.views import main_first_window, phon_scan
+from app.views import main_first_window, phon_scan, active_main_window
 from . import view
 # from app.views import *
 
@@ -9,6 +9,7 @@ from . import view
 urlpatterns = [
     path('phone_scan/', phon_scan.scan, name='phon_scan'),
     path('start/', main_first_window.open_window, name='open_window'),
-    
+    path('active/', active_main_window.active_window, name='active_window'),
+
 ]
 
