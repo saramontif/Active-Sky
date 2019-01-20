b180b72b76bd6220fece170583882e70eb6cac3c
saramontif <saramontif@gmail.com>
2019-01-20 16:54:45 +0200

    merge

--- Flight/Flight/urls.py
+++ Flight/Flight/urls.py
@@ -1,15 +1,13 @@
-# from django.contrib import admin
 from django.urls import path
-
 from app.views import main_first_window, phon_scan, active_main_window
 from app.views.main_first_window import home_view
 from . import view
 
 
-urlpatterns = [
+
+urlpatterns =[
     path('phone_scan/', phon_scan.scan, name='phon_scan'),
     path('start/', home_view.as_view(), name='start'),
     path('active/', active_main_window.active_window, name='active_window'),
-
 ]
 
