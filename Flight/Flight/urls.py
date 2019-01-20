b4a13e71da33c03ea2dcb1c82bfe656891b949b6
sariVagshal <sari6910@gmail.com>
2019-01-20 17:35:13 +0200

    nicer name of URL

--- Flight/Flight/urls.py
+++ Flight/Flight/urls.py
@@ -2,13 +2,7 @@ from django.urls import path
 
 from core.views import active_main_window
 from core.views import phon_scan
-# from app.views import main_first_window, phon_scan, active_main_window
 from core.views.main_first_window import home_view
-# =======
-# from app.views import main_first_window, phon_scan, active_main_window
-# from app.views.main_first_window import home_view
-# from . import view
-# >>>>>>> b180b72b76bd6220fece170583882e70eb6cac3c
 
 
 
