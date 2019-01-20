82470181c734c6451c590fcb78e9cb9ec5244458
saramontif <saramontif@gmail.com>
2019-01-20 12:23:07 +0200

    ex of qr

---
+++ Flight/Flight/view.py
@@ -0,0 +1,14 @@
+import qrcode
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+data = [u"geeksforgeeks", u"https://www.geeksforgeeks.org/"]
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+print(img)
+img.save("image.jpg")
