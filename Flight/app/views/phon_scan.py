2651f3a7e982148a80018b170a6dda4bbe5be022
Yael Yazdi <yael.yazdi96@gmail.com>
2019-01-20 15:29:59 +0200

    static JS + CSS

--- Flight/app/views/phon_scan.py
+++ Flight/app/views/phon_scan.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 
 def scan(request):
-    some = "something to see\nHi!"
-    return HttpResponse(some, content_type="text/plain")
+    # some = "something to see\nHi!"
+    return render(request, 'templates/Flight/phon_scan.html', {})
 
 # def phon_scan(request):
 #     return render(request, 'app/phon_scan.html', {})
