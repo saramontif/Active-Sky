27acef9863ba0b69a57bec7a545339bd09a47025
Yael Yazdi <yael.yazdi96@gmail.com>
2019-01-21 11:36:36 +0200

    get the text from the user DONE 🤩🤩

--- Flight/core/views.py
+++ Flight/core/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+
+from view.phone_scan import ScanForm
+
+def get_name(request):
+    if request.method == 'POST':
+        form = ScanForm(request.POST)
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/text/')
+        print(form)
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ScanForm()
+
+    # return render(request, 'name.html', {'form': form})
+
+
+
