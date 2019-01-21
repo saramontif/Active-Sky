28dc9b46dedc30c87c0d568989500c3d2c32d127
Yael Yazdi <yael.yazdi96@gmail.com>
2019-01-21 09:45:10 +0200

    HTML DESIN PHONE SCAN 😉

--- Flight/core/views/phone_scan.py
+++ Flight/core/views/phone_scan.py
@@ -2,11 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-
-# def scan(request):
-#     # some = "something to see\nHi!"
-#     return HttpResponse(render(request, "phone_scan.html",), content_type="text/plain")
-
 class scan(TemplateView):
     template_name = 'phone_scan.html'
 
