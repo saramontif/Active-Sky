b12873fb34fbb8537aa947bd3d136ebfa1b3de04
sariVagshal <sari6910@gmail.com>
2019-01-21 12:07:39 +0200

    merge the HTML and the functions

--- Flight/core/views/active_main_window.py
+++ Flight/core/views/active_main_window.py
@@ -1,6 +1,20 @@
 from django.views.generic import TemplateView
-
+import re
+import urllib.parse, urllib.request
 
 
 class active_view(TemplateView):
     template_name = 'active_main.html'
+
+    def get_url(self):
+        place = "Isreali museon"
+        query_string = urllib.parse.urlencode({"search_query": place})
+        html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+        search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+        return f"https://www.youtube.com/embed/{search_results[0]}"
+
+    def get_topic(self):
+        return "TOPIC..."
+
+    def get_seat(self):
+        return "YOUR COMMENT: ..."
