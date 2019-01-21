00167435d21ae931f51a1f0df82b287ee4b6ffc5
sariVagshal <sari6910@gmail.com>
2019-01-21 16:34:20 +0200

    between screens

--- Flight/core/views/main_first_window.py
+++ Flight/core/views/main_first_window.py
@@ -1,7 +1,26 @@
+from django.http import JsonResponse
+from django.views import View
 from django.views.generic import TemplateView
 
-#
-class home_view(TemplateView):
+flag = False
+
+def event():
+    global flag
+    flag = True
+
+def get_flag():
+    return flag
+
+
+class HomeView(TemplateView):
     template_name = 'first_main_html.html'
 
 
+# def flag_view(request):
+#     return JsonResponse({'flag': get_flag()})
+
+
+class FlagView(View):
+    def get(self, request, *args, **kwargs):
+        flag0 = get_flag()
+        return JsonResponse({'flag': flag0})
--- Flight/core/views/phone_scan.py
+++ Flight/core/views/phone_scan.py
@@ -2,6 +2,7 @@ from django import forms
 from django.views.generic import TemplateView, FormView
 
 from core.views.active_main_window import get_data_from_user
+from core.views.main_first_window import event
 
 
 class ScanForm(forms.Form):
@@ -16,6 +17,7 @@ class ScanView(FormView):
     def form_valid(self, form):
         d = form.cleaned_data
         # return d
+        event()
         get_data_from_user(d)
 
     def form_invalid(self, form):
